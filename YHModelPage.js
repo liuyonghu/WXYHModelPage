5e68d21fcbeace1f208534f5be06d7258cfcaa1e
liuyonghu <you@example>
2019-02-14 16:06:13 +0800

    del comments

--- YHModelPage.js
+++ YHModelPage.js
@@ -25,11 +25,9 @@ Component({
                 // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串, 如：'_propertyChange'
                 // 通常 newVal 就是新设置的数据， oldVal 是旧数据
                 if (newVal) {
-                    console.log("   if(newVal){");
                     this.showYHModelPage();
                 } else {
                     this.closePage();
-                    console.log("   if(newVal){ }else{");
                 }
             }
         },
@@ -74,7 +72,7 @@ Component({
                         showClose: newVal.showClose || false,
                         YHModelViewInfoData: newVal
                     });
-                    console.log("   if(newVal){ = =" + JSON.stringify(newVal));
+                //     console.log("   if(newVal){ = =" + JSON.stringify(newVal));
 
                 } else {
 
